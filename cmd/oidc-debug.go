fcd7dc2ed66433caaa0e0737090a701d7a8d533b
amanstep <aman@stepsecurity.io>
2025-10-08 23:00:39 +0530

    fix: addressed PR comments

--- cmd/oidc-debug.go
+++ cmd/oidc-debug.go
@@ -3,36 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
-	"os"
-	"time"
 
 	"github.com/github/actions-oidc-debugger/actionsoidc"
 )
 
-func validateSubscription() {
-	repo := os.Getenv("GITHUB_REPOSITORY")
-	apiURL := fmt.Sprintf("https://agent.api.stepsecurity.io/v1/github/%s/actions/subscription", repo)
-
-	client := http.Client{
-		Timeout: 3 * time.Second,
-	}
-
-	resp, err := client.Get(apiURL)
-	if err != nil {
-		fmt.Println("Timeout or API not reachable. Continuing to next step.")
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusForbidden {
-		fmt.Fprintln(os.Stderr, "Subscription is not valid. Reach out to support@stepsecurity.io")
-		os.Exit(1)
-	}
-}
-
 func main() {
-	validateSubscription()
 	audience := flag.String("audience", "https://github.com/", "the audience for the requested jwt")
 	flag.Parse()
 
